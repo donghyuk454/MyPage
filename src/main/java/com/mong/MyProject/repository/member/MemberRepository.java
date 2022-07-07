abc76d156c39d57dacdda2dae495a564c997f1bf
dongdong <donghyuk454@gmail.com>
2022-07-07 22:16:29 +0900

    feat: member repository 기능 추가

--- src/main/java/com/mong/MyProject/repository/member/MemberRepository.java
+++ src/main/java/com/mong/MyProject/repository/member/MemberRepository.java
@@ -2,6 +2,7 @@ package com.mong.MyProject.repository.member;
 
 import com.mong.MyProject.domain.member.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository {
@@ -9,4 +10,6 @@ public interface MemberRepository {
     Optional<Member> findById(Long id);
     Optional<Member> findByEmailAndPasswd(String email, String passwd);
     Optional<Member> findByAlias(String alias);
+    Optional<Member> findByEmail(String email);
+    List<Member> findAll();
 }
--- src/main/java/com/mong/MyProject/repository/member/MemberRepositoryImpl.java
+++ src/main/java/com/mong/MyProject/repository/member/MemberRepositoryImpl.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -46,5 +47,20 @@ public class MemberRepositoryImpl implements MemberRepository {
                 .getResultList()
                 .get(0);
         return Optional.ofNullable(member);
+    }
+
+    @Override
+    public Optional<Member> findByEmail(String email) {
+        Member member = em.createQuery("select m from member m where m.email = :email", Member.class)
+                .setParameter("email", email)
+                .getResultList()
+                .get(0);
+        return Optional.ofNullable(member);
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return em.createQuery("select m from member m", Member.class)
+                .getResultList();
     }
 }
