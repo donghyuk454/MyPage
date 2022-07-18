db145731b5e2b3ad5c35ea661b6690e71ad08256
dongdong <donghyuk454@gmail.com>
2022-07-18 14:20:00 +0900

    feat: image 에 builder 모델 적용

--- src/main/java/com/mong/MyProject/domain/image/Image.java
+++ src/main/java/com/mong/MyProject/domain/image/Image.java
@@ -19,5 +19,30 @@ public class Image {
     private Long id;
 
     @Column(name = "image_url", nullable = false, unique = true)
-    protected String url;
+    private String url;
+
+    @Column(name = "key", nullable = false)
+    private String key;
+
+    @Column(name = "type")
+    @Enumerated(value = EnumType.STRING)
+    private ImageType type;
+
+    @Builder
+    public Image(Long id, String url, ImageType type, Board board, Member member) {
+        this.id = id;
+        this.url = url;
+        this.type = type;
+        if (type.equals(ImageType.BOARD)) {
+            board.addImage(this);
+        } else if (type.equals(ImageType.MEMBER)) {
+            member.setImage(this);
+        }
+
+        this.key = extractKey();
+    }
+
+    private String extractKey(){
+        return "";
+    }
 }
