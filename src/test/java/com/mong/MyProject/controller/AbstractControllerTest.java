06935e4bf178e55e771f52895f2269f75ed89388
dongdong <donghyuk454@gmail.com>
2022-07-20 21:35:20 +0900

    feat: member 와 board controller test 에서 공통으로 사용하는 내용을 추상화

---
+++ src/test/java/com/mong/MyProject/controller/AbstractControllerTest.java
@@ -0,0 +1,23 @@
+package com.mong.MyProject.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+@ExtendWith(SpringExtension.class)
+public abstract class AbstractControllerTest {
+
+    protected MockMvc mockMvc;
+
+    @BeforeEach
+    void beforeEach(){
+        mockMvc = MockMvcBuilders.standaloneSetup(setController())
+                .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .build();
+    }
+
+    protected abstract Object setController();
+}
