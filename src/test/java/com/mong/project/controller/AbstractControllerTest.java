5b5b31f7fb08810997d58b5a7f02cec49500c5a5
dongdong <donghyuk454@gmail.com>
2022-08-10 22:56:30 +0900

    feat: rest doc 설정 및 기능 추가

--- src/test/java/com/mong/project/controller/AbstractControllerTest.java
+++ src/test/java/com/mong/project/controller/AbstractControllerTest.java
@@ -5,24 +5,46 @@ import com.mong.project.exception.ExceptionHandlers;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.operation.preprocess.OperationRequestPreprocessor;
+import org.springframework.restdocs.operation.preprocess.OperationResponsePreprocessor;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
-@ExtendWith(SpringExtension.class)
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@AutoConfigureRestDocs
+@AutoConfigureMockMvc
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 public abstract class AbstractControllerTest {
 
     protected MockMvc mockMvc;
     protected Gson gson = new Gson();
 
     @BeforeEach
-    void beforeEach(){
+    void beforeEach(RestDocumentationContextProvider restDocumentationContextProvider){
         mockMvc = MockMvcBuilders.standaloneSetup(setController())
                 .addFilter(new CharacterEncodingFilter("UTF-8", true))
+                .apply(documentationConfiguration(restDocumentationContextProvider))
+                .alwaysDo(print())
                 .setControllerAdvice(new ExceptionHandlers())
                 .build();
     }
 
     protected abstract Object setController();
+
+    public OperationRequestPreprocessor requestPreprocessor() {
+        return preprocessRequest(prettyPrint());
+    }
+
+    public OperationResponsePreprocessor responsePreprocessor() {
+        return preprocessResponse(prettyPrint());
+    }
 }
