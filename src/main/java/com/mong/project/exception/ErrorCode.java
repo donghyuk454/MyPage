6c1b2161b00b3269afea68322383bb4ee31137cb
dongdong <donghyuk454@gmail.com>
2022-08-16 16:35:37 +0900

    refactor: 에러코드 추가 (invalid session)

--- src/main/java/com/mong/project/exception/ErrorCode.java
+++ src/main/java/com/mong/project/exception/ErrorCode.java
@@ -20,4 +20,7 @@ public class ErrorCode {
     // image
     public static final String FAIL_TO_WRITE_FILE = "파일 작성에 실패하였습니다.";
     public static final String FAIL_TO_REMOVE_FILE = "파일 삭제에 실패하였습니다.";
+
+    //session
+    public static final String INVALID_SESSION = "세션";
 }
