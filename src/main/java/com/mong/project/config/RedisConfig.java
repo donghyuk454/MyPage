059d185a357d3218ac61a47c58bdd3f9eb108d40
dongdong <donghyuk454@gmail.com>
2022-08-16 16:40:56 +0900

    chore: redis 설정 추가

---
+++ src/main/java/com/mong/project/config/RedisConfig.java
@@ -0,0 +1,58 @@
+package com.mong.project.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisPassword;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
+
+@Configuration
+@EnableRedisHttpSession
+public class RedisConfig extends AbstractHttpSessionApplicationInitializer {
+
+    private final String host;
+
+    private final Integer port;
+
+    private final String password;
+
+    private final ObjectMapper objectMapper;
+
+    public RedisConfig(@Value("${spring.redis.host}") String host,
+                       @Value("${spring.redis.port}") Integer port,
+                       @Value("${spring.redis.password}") String password,
+                       ObjectMapper objectMapper) {
+        this.host = host;
+        this.port = port;
+        this.password = password;
+        this.objectMapper = objectMapper;
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setEnableTransactionSupport(true);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper));
+
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(){
+        RedisStandaloneConfiguration standaloneConfiguration = new RedisStandaloneConfiguration(host, port);
+        standaloneConfiguration.setPassword(password.isEmpty() ? RedisPassword.none() : RedisPassword.of(password));
+        return new LettuceConnectionFactory(standaloneConfiguration);
+    }
+}
