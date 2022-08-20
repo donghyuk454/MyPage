2fa8d9a7929604e0503fe9a7f566d1472194a5b2
dongdong <donghyuk454@gmail.com>
2022-08-20 19:28:20 +0900

    feat: readme 작성을 위한 controller 제작

---
+++ src/main/java/com/mong/project/controller/ReadmeController.java
@@ -0,0 +1,39 @@
+package com.mong.project.controller;
+
+import com.mong.project.util.readme.ReadmeWriter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@Profile("default")
+@PropertySource("classpath:application.properties")
+public class ReadmeController {
+
+    private final ReadmeWriter readmeWriter;
+
+    private final String csvPath;
+
+    private final String readmePath;
+
+    public ReadmeController(ReadmeWriter readmeWriter,
+                            @Value("${spring.readme.path}") String readmePath,
+                            @Value("${spring.test.csv.path}") String csvPath){
+        this.readmeWriter = readmeWriter;
+        this.readmePath = readmePath;
+        this.csvPath = csvPath;
+    }
+
+    @GetMapping("/readme")
+    public ResponseEntity<Void> writeReadme() {
+        if (!readmeWriter.writeReadme(readmePath, csvPath))
+            return ResponseEntity.badRequest().build();
+
+        return ResponseEntity.ok().build();
+    }
+}
