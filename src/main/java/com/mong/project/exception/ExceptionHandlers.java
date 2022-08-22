09aa5d310b24cfcf52352ac310d1353f9a666a9c
dongdong <donghyuk454@gmail.com>
2022-08-22 21:57:21 +0900

    feat: Exception handler 에서 session exception 도 처리

--- src/main/java/com/mong/project/exception/ExceptionHandlers.java
+++ src/main/java/com/mong/project/exception/ExceptionHandlers.java
@@ -3,6 +3,7 @@ package com.mong.project.exception;
 import com.mong.project.dto.response.ErrorResponse;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -16,7 +17,7 @@ import java.util.NoSuchElementException;
 public class ExceptionHandlers {
 
     @ResponseBody
-    @ExceptionHandler({NoSuchElementException.class, EntityNotFoundException.class, IllegalStateException.class, IllegalArgumentException.class})
+    @ExceptionHandler({SessionException.class, NoSuchElementException.class, EntityNotFoundException.class, IllegalStateException.class, IllegalArgumentException.class})
     public ResponseEntity<ErrorResponse> handleException(RuntimeException e){
         log.info("에러 발생 message = {}", e.getMessage());
         return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
