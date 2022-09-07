bca8e89fa8cddcef1b40234dc4ec7952a5e891ca
dongdong <donghyuk454@gmail.com>
2022-09-07 16:18:21 +0900

    feat: readme write 자동화

---
+++ scripts/testBuild.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LOG_PATH="C:\Users\dongh\spring-project\MyProject\scripts\log\testBuild.log"
+rm ${LOG_PATH}
+sleep 2
+
+cd C:/Users/dongh/spring-project/MyProject
+
+echo "> test 시작" >> ${LOG_PATH}
+./gradlew test >> ${LOG_PATH}
+sleep 5
+
+echo "> server 구동" >> ${LOG_PATH}
+nohup ./gradlew bootRun >> ${LOG_PATH} &
+sleep 30
+
+echo "> readme 작성" >> ${LOG_PATH}
+
+curl -I http://localhost:9000/readme >> ${LOG_PATH} &
+
+echo "> server 종료" >> ${LOG_PATH}
+./gradlew --stop >> ${LOG_PATH}
+rm -rf %USER_HOME%\.gradle/caches/ >> ${LOG_PATH}
