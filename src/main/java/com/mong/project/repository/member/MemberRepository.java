7a62e47552b7770d7ed561d7213626e7c2bfe799
donghyuk <donghyuk454@gmail.com>
2023-06-30 00:53:27 +0900

    [FEAT] Spring Data Jpa 를 적용해 기존의 member repository 기능 구현

--- src/main/java/com/mong/project/repository/member/MemberRepository.java
+++ src/main/java/com/mong/project/repository/member/MemberRepository.java
@@ -1,15 +1,14 @@
 package com.mong.project.repository.member;
 
 import com.mong.project.domain.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository {
-    Member save(Member member);
-    Optional<Member> findById(Long id);
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByAlias(String alias);
     Optional<Member> findByEmail(String email);
     Optional<Member> findByEmailAndAlias(String email, String alias);
-    List<Member> findAll();
 }
