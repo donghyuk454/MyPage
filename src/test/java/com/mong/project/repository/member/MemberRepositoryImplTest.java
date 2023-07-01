bed0082cd19c974938ae505ddb2ca31fdff4a338
donghyuk <donghyuk454@gmail.com>
2023-07-01 22:05:10 +0900

    [TEST] 테스트 환경을 dev 환경으로 변경

--- src/test/java/com/mong/project/repository/member/MemberRepositoryImplTest.java
+++ src/test/java/com/mong/project/repository/member/MemberRepositoryImplTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,6 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("dev")
 @Transactional
 class MemberRepositoryImplTest {
 
