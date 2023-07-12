a98c8968fbfa6ff6bd9f75973354b6dd22ccd5ec
donghyuk <donghyuk454@gmail.com>
2023-07-12 23:34:20 +0900

    [REFACTOR] save 함수 변경에 따른 테스트 코드 수정, member 와 board 를 실제로 생성하도록 로직 변경 후 공통 처리

--- src/test/java/com/mong/project/repository/comment/CommentRepositoryImplTest.java
+++ src/test/java/com/mong/project/repository/comment/CommentRepositoryImplTest.java
@@ -4,48 +4,66 @@ import com.mong.project.domain.board.Board;
 import com.mong.project.domain.comment.Comment;
 import com.mong.project.domain.member.Member;
 import com.mong.project.repository.board.BoardRepository;
+import com.mong.project.repository.member.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
+@ActiveProfiles("dev")
 @Transactional
 class CommentRepositoryImplTest {
 
     @Autowired private CommentRepository commentRepository;
     @Autowired private BoardRepository boardRepository;
+    @Autowired private MemberRepository memberRepository;
+    @PersistenceContext
+    private EntityManager em;
 
     @Test
     @DisplayName("새로운 comment 를 저장합니다. 그리고 해당 comment 를 반환합니다.")
     void createComment() {
         //given
-        Member member = Member.builder().build();
-        Board board = Board.builder().build();
+        Member member = memberRepository.save(createMember());
+        Board board = boardRepository.save(createBoard(member));
 
         //when
-        Comment comment = commentRepository.save(member, board, "content");
+        Comment comment = createNewComment(member, board);
+        Comment result = commentRepository.save(comment);
 
         //then
-        assertThat(comment.getId()).isNotNull();
-        assertThat(member).isEqualTo(comment.getMember());
-        assertThat(board).isEqualTo(comment.getBoard());
-        assertThat(board.getComments().contains(comment)).isTrue();
+        assertThat(result.getId()).isNotNull();
+        assertThat(member).isEqualTo(result.getMember());
+        assertThat(board).isEqualTo(result.getBoard());
+        assertThat(board.getComments()).contains(result);
+    }
+
+    private static Comment createNewComment(Member member, Board board) {
+        return Comment.builder()
+                .content("content")
+                .member(member)
+                .board(board)
+                .build();
     }
 
     @Test
     @DisplayName("comment_id 를 통해 comment 를 조회합니다. 조회한 comment 를 반환합니다.")
     void getComment() {
         //given
-        Member member = Member.builder().build();
-        Board board = Board.builder().build();
-        Comment comment = commentRepository.save(member, board, "content");
+        Member member = memberRepository.save(createMember());
+        Board board = boardRepository.save(createBoard(member));
+        Comment comment = commentRepository.save(createNewComment(member, board));
 
         //when
         Comment result = commentRepository.findById(comment.getId()).get();
@@ -60,14 +78,34 @@ class CommentRepositoryImplTest {
     @DisplayName("comment_id 를 통해 comment 를 삭제합니다. 이 때, board 의 comments 에서도 삭제되었는지 확인합니다.")
     void deleteComment() {
         //given
-        Member member = Member.builder().build();
-        Board board = boardRepository.save(Board.builder().build());
-        Comment comment = commentRepository.save(member, board, "content");
+        Member member = memberRepository.save(createMember());
+        Board board = boardRepository.save(createBoard(member));
+        Comment comment = commentRepository.save(createNewComment(member, board));
 
         //when
         commentRepository.deleteById(comment.getId());
+        em.flush();
+        em.clear();
 
         //then
-        assertThat(board.getComments().size()).isZero();
+
+        assertThat(board.getComments()).isEmpty();
+    }
+
+    private static Board createBoard(Member member) {
+        return Board.builder()
+                .title("test title")
+                .content("test content")
+                .member(member)
+                .build();
+    }
+
+    private static Member createMember() {
+        return Member.builder()
+                .name("test")
+                .alias("test")
+                .email("test@test.com")
+                .passwd("testpasswd")
+                .build();
     }
 }
