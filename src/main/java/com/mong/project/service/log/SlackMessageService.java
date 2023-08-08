fa86870abd3039445352b16bae1d28e412e9d26b
donghyuk <donghyuk454@gmail.com>
2023-08-08 23:55:58 +0900

    [FEAT] slack 을 사용한 메시지 전송 기능 개발

--- src/main/java/com/mong/project/service/log/SlackMessageService.java
+++ src/main/java/com/mong/project/service/log/SlackMessageService.java
@@ -1,16 +1,46 @@
 package com.mong.project.service.log;
 
-import com.mong.project.service.log.dto.MessageDto;
+import com.slack.api.Slack;
+import com.slack.api.methods.MethodsClient;
+import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+
 @Service
+@Slf4j
+@PropertySource("classpath:application.properties")
 public class SlackMessageService implements MessageService {
+
+    private final String token;
+    private final String channel;
+
+    public SlackMessageService(@Value("${spring.slack.token}") String token,
+                               @Value("${spring.slack.channel}") String channel) {
+        this.token = token;
+        this.channel = channel;
+    }
+
     @Override
-    public MessageDto sendMessage(String title, String message) {
-        MessageDto messageDto = new MessageDto(title, message);
-
+    public Boolean sendMessage(String text) {
         // message 전송 로직
+        MethodsClient client = Slack.getInstance().methods();
+        try {
+            ChatPostMessageResponse chatPostMessageResponse = client
+                    .chatPostMessage(requestBuilder -> requestBuilder
+                            .token(token)
+                            .channel(channel)
+                            .text(text));
 
-        return messageDto;
+            return chatPostMessageResponse.isOk();
+
+        } catch (IOException | SlackApiException e) {
+            log.error("Slack error: {}", e.getMessage(), e);
+            return false;
+        }
     }
 }
