365fa7009211c426f41a9f255c4bd98a761a6501
donghyuk <donghyuk454@gmail.com>
2023-08-15 19:27:22 +0900

    [REFACTOR] test 작성을 용이하게 하기 위해 client 를 외부에서 주입할 수 있는 생성자 생성 및 로직 보완

--- src/main/java/com/mong/project/service/log/SlackMessageService.java
+++ src/main/java/com/mong/project/service/log/SlackMessageService.java
@@ -3,8 +3,10 @@ package com.mong.project.service.log;
 import com.slack.api.Slack;
 import com.slack.api.methods.MethodsClient;
 import com.slack.api.methods.SlackApiException;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
@@ -18,23 +20,27 @@ public class SlackMessageService implements MessageService {
 
     private final String token;
     private final String channel;
+    private final MethodsClient client;
 
+    @Autowired
     public SlackMessageService(@Value("${spring.slack.token}") String token,
                                @Value("${spring.slack.channel}") String channel) {
+        this(token, channel, Slack.getInstance().methods());
+    }
+
+    public SlackMessageService(String token, String channel, MethodsClient client) {
         this.token = token;
         this.channel = channel;
+        this.client = client;
     }
 
     @Override
     public Boolean sendMessage(String text) {
-        // message 전송 로직
-        MethodsClient client = Slack.getInstance().methods();
         try {
+            ChatPostMessageRequest request = createRequest(text);
+
             ChatPostMessageResponse chatPostMessageResponse = client
-                    .chatPostMessage(requestBuilder -> requestBuilder
-                            .token(token)
-                            .channel(channel)
-                            .text(text));
+                    .chatPostMessage(request);
 
             return chatPostMessageResponse.isOk();
 
@@ -42,5 +48,13 @@ public class SlackMessageService implements MessageService {
             log.error("Slack error: {}", e.getMessage(), e);
             return false;
         }
+    }
+
+    private ChatPostMessageRequest createRequest(String text) {
+        return ChatPostMessageRequest.builder()
+                .token(token)
+                .channel(channel)
+                .text(text)
+                .build();
     }
 }
