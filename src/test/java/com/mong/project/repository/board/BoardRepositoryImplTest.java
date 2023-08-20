25c994b240154b82418a40010d56b37d24315be6
donghyuk <donghyuk454@gmail.com>
2023-08-20 23:41:16 +0900

    [REFACTOR] boot test 를 jpa test 로 변경, 테스트 코드 중 불필요 코드 제거, 테스트 검증조건 추가 및 함수명 수정

--- src/test/java/com/mong/project/repository/board/BoardRepositoryImplTest.java
+++ src/test/java/com/mong/project/repository/board/BoardRepositoryImplTest.java
@@ -5,14 +5,15 @@ import com.mong.project.domain.board.BoardStatus;
 import com.mong.project.domain.member.Member;
 import com.mong.project.repository.member.MemberRepository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -20,26 +21,31 @@ import javax.persistence.PersistenceContext;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
 @ActiveProfiles("dev")
-@Transactional
+@DataJpaTest
+@Rollback(value = false)
 class BoardRepositoryImplTest {
 
     @Autowired private BoardRepository boardRepository;
     @Autowired private MemberRepository memberRepository;
     @PersistenceContext private EntityManager em;
 
+    @BeforeEach
+    void setup() {
+        boardRepository.deleteAll();
+        memberRepository.deleteAll();
+        em.flush();
+        em.clear();
+    }
+
     @Test
     @DisplayName("새로운 board 에 영속성을 부여합니다.")
     void addBoard() {
         //given
-        Member member = newTestMember();
-        Long memberId = member.getId();
+        Member member = createTestMember();
 
         //when
-        member = memberRepository.getById(memberId);
-        Board board = newTestBoard();
-        member.addBoard(board);
+        Board board = createTestBoard(member);
 
         //then
         assertThat(board.getMember().getId()).isEqualTo(member.getId());
@@ -51,20 +57,17 @@ class BoardRepositoryImplTest {
     @DisplayName("board 의 내용을 수정합니다. member 에서 board 를 가져와 수정하고 저장할 때 문제가 없는지 확인합니다.")
     void changeBoard() {
         //given
-        Member member = newTestMember();
-        member.addBoard(newTestBoard());
-        memberRepository.save(member);
+        Member member = createTestMember();
+        Board board = createTestBoard(member);
+        member.addBoard(board);
 
         //when
-        Board board = member.getBoards().get(0);
         board.setContent("수정된 내용입니다");
         board.setTitle("수정된 제목입니다");
-        boardRepository.save(board);
-
-        Board result1 = boardRepository.findById(board.getId()).get();
-        Board result2 = member.getBoards().get(0);
 
         //then
+        Board result1 = boardRepository.findById(board.getId()).get();
+        Board result2 = member.getBoards().get(0);
         assertThat(result1).isNotNull();
         isEqualBoard(result1, result2);
     }
@@ -73,6 +76,7 @@ class BoardRepositoryImplTest {
         assertThat(result2.getId()).isEqualTo(result1.getId());
         assertThat(result2.getTitle()).isEqualTo(result1.getTitle());
         assertThat(result2.getContent()).isEqualTo(result1.getContent());
+        assertThat(result2.getMember()).isEqualTo(result1.getMember());
     }
 
 
@@ -80,10 +84,8 @@ class BoardRepositoryImplTest {
     @DisplayName("board id 를 통해 board 를 조회합니다.")
     void getBoard() {
         //given
-        Member member = newTestMember();
-        member.addBoard(newTestBoard());
-        memberRepository.save(member);
-        Board board = member.getBoards().get(0);
+        Member member = createTestMember();
+        Board board = createTestBoard(member);
 
         //when
         Board result = boardRepository.findById(board.getId()).get();
@@ -97,8 +99,8 @@ class BoardRepositoryImplTest {
     @DisplayName("board_id 를 통해 board 를 삭제합니다. member 의 boards 에서도 삭제가 되는지 확인합니다.")
     void deleteBoard() {
         //given
-        Member member = newTestMember();
-        Board board = newTestBoard();
+        Member member = createTestMember();
+        Board board = createTestBoard(member);
         member.addBoard(board);
 
         //when
@@ -110,16 +112,20 @@ class BoardRepositoryImplTest {
         assertThat(board.getDeletedDateTime()).isNotNull();
     }
 
-    private Member newTestMember(){
-        Member member = memberRepository.save(Member.builder()
-                .name("name").email("tt@test.com").alias("테스트 닉네임").passwd("passwd").build());
-        em.flush();
-        em.clear();
-        return member;
+    private Member createTestMember(){
+        return memberRepository.save(Member.builder()
+                .name("testName")
+                .email("no-reply@test.com")
+                .alias("테스트 닉네임")
+                .passwd("passwd")
+                .build());
     }
 
-    private Board newTestBoard() {
+    private Board createTestBoard(Member member) {
         return boardRepository.save(Board.builder()
-                .title("제목입니다").content("내용입니다.").build());
+                .member(member)
+                .title("제목입니다")
+                .content("내용입니다.")
+                .build());
     }
 }
