cc79cddc7f3fbb33e05dda97c5f01cced98fdf82
donghyuk <donghyuk454@gmail.com>
2023-09-05 22:58:39 +0900

    [TEST] Custom Exception 정책 적용 및 테스트 코드 추가

--- src/test/java/com/mong/project/util/log/aspect/LogAspectTest.java
+++ src/test/java/com/mong/project/util/log/aspect/LogAspectTest.java
@@ -1,6 +1,8 @@
 package com.mong.project.util.log.aspect;
 
 import com.mong.project.domain.member.Member;
+import com.mong.project.exception.MyPageException;
+import com.mong.project.exception.UncheckedException;
 import com.mong.project.repository.member.MemberRepository;
 import com.mong.project.util.log.service.ServerExceptionLogService;
 import com.mong.project.util.log.service.dto.ExceptionLogDto;
@@ -13,7 +15,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.aop.aspectj.annotation.AspectJProxyFactory;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import javax.persistence.EntityNotFoundException;
+
+import static com.mong.project.exception.ErrorCode.UNCHECKED_EXCEPTION;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.mockito.Mockito.*;
 import static org.mockito.Mockito.mock;
 
@@ -38,25 +43,24 @@ class LogAspectTest {
         factory.addAspect(logAspect);
     }
 
-    @DisplayName("Exception 이 발생하면 LogAspect 의 sendLogMessage 함수가 실행됩니다.")
+    @DisplayName("RuntimeException 이 발생하면 LogAspect 의 sendLogMessage 함수가 실행됩니다. 최종적으로 UncheckedException 을 throw 합니다.")
     @Test
-    void sendMessageWhenThereIsAnException() {
+    void sendMessageWhenThereIsRuntimeException() {
         //given
         String message = "message";
-        Exception exception = new IllegalStateException(message);
+        Exception exception = new RuntimeException(message);
         when(memberRepository.save(mockMember))
                 .thenThrow(exception);
 
         MemberRepository repository = factory.getProxy();
 
         //when
-        try {
-            repository.save(mockMember);
-        } catch (IllegalStateException e) {
-            assertThat(e).isEqualTo(exception);
-        }
+        Throwable throwable = catchThrowable(() -> repository.save(mockMember));
 
         //then
+        assertThat(throwable.getCause()).isEqualTo(exception);
+        assertThat(throwable.getMessage()).isEqualTo(UNCHECKED_EXCEPTION);
+        assertThat(throwable.getClass()).isEqualTo(UncheckedException.class);
         verify(messageService,
                 times(1))
                 .sendMessage(anyString());
@@ -65,7 +69,7 @@ class LogAspectTest {
                 .addExceptionLog(any(ExceptionLogDto.class));
     }
 
-    @DisplayName("Exception 이 발생하면 LogAspect 의 sendLogMessage 함수가 실행됩니다.")
+    @DisplayName("Exception 이 발생하지 않으면 정상적으로 기능을 수행합니다.")
     @Test
     void sendNoMessageWhenThereIsNoException() {
         //given
@@ -78,6 +82,55 @@ class LogAspectTest {
         repository.save(mockMember);
 
         //then
+        verify(messageService,
+                times(0))
+                .sendMessage(anyString());
+        verify(exceptionLogService,
+                times(0))
+                .addExceptionLog(any(ExceptionLogDto.class));
+    }
+
+    @DisplayName("MyPageException 이 발생하면 LogAspect 의 sendLogMessage 함수가 실행되지 않으며 그대로 throw 합니다.")
+    @Test
+    void sendNoMessageWhenThereIsMyPageException() {
+        //given
+        String message = "message";
+        Exception exception = new MyPageException(message);
+        when(memberRepository.save(mockMember))
+                .thenThrow(exception);
+
+        MemberRepository repository = factory.getProxy();
+
+        //when
+        Throwable throwable = catchThrowable(() -> repository.save(mockMember));
+
+        //then
+        assertThat(throwable).isEqualTo(exception);
+        verify(messageService,
+                times(0))
+                .sendMessage(anyString());
+        verify(exceptionLogService,
+                times(0))
+                .addExceptionLog(any(ExceptionLogDto.class));
+    }
+
+    @DisplayName("PersistenceException 이 발생하면 LogAspect 의 sendLogMessage 함수가 실행되지 않으며, MyPageException 으로 throw 합니다.")
+    @Test
+    void sendNoMessageWhenThereIsPersistenceException() {
+        //given
+        String message = "message";
+        Exception exception = new EntityNotFoundException(message);
+        when(memberRepository.save(mockMember))
+                .thenThrow(exception);
+
+        MemberRepository repository = factory.getProxy();
+
+        //when
+        Throwable throwable = catchThrowable(() -> repository.save(mockMember));
+
+        //then
+        assertThat(throwable.getCause()).isEqualTo(exception);
+        assertThat(throwable.getClass()).isEqualTo(MyPageException.class);
         verify(messageService,
                 times(0))
                 .sendMessage(anyString());
